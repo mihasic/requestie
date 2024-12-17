90249deb49fb16a2324730c7c1fc9c89558a9a8a
Mike Rybnikov <mihasic@gmail.com>
2024-12-17 18:54:37 +0100

    some wip on ui

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,61 @@
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct RequestieApp {
-    // Example stuff:
-    label: String,
+use strum::{Display, EnumIter, EnumString, IntoEnumIterator};
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+#[derive(
+    PartialEq, Debug, Clone, serde::Serialize, serde::Deserialize, Display, EnumString, EnumIter,
+)]
+pub enum HttpMethod {
+    GET,
+    HEAD,
+    PUT,
+    POST,
+    PATCH,
+    DELETE,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+struct Request {
+    name: String,
+    url: String,
+    method: HttpMethod,
+    headers: Vec<(String, String)>,
+    body: String,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+struct Environment {
+    name: String,
+    values: Vec<(String, String)>,
+}
+
+#[derive(Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+enum PanelSelection {
+    Request(usize),
+    Environment(usize),
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+#[serde(default)]
+pub struct RequestieApp {
+    requests: Vec<Request>,
+    environments: Vec<Environment>,
+    selected_panel: PanelSelection,
 }
 
 impl Default for RequestieApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            requests: vec![Request {
+                name: "Request 1".to_string(),
+                url: "https://example.com".to_string(),
+                method: HttpMethod::GET,
+                headers: vec![("Content-Type".to_string(), "application/json".to_string())],
+                body: "".to_string(),
+            }],
+            environments: vec![Environment {
+                name: "Default".to_string(),
+                values: vec![],
+            }],
+            selected_panel: PanelSelection::Request(0),
         }
     }
 }
@@ -32,6 +74,119 @@ impl RequestieApp {
 
         Default::default()
     }
+
+    fn show_request_editor(&mut self, ui: &mut eframe::egui::Ui, idx: usize) {
+        let request = &mut self.requests[idx];
+
+        egui::TopBottomPanel::top("header")
+            .show_inside(ui, |ui| {
+                ui.heading(&request.name);
+                ui.horizontal(|ui| {
+                    ui.text_edit_singleline(&mut request.name);
+                });
+        
+            });
+
+        ui.horizontal(|ui| {
+            eframe::egui::ComboBox::from_label("")
+                .selected_text(request.method.to_string())
+                .show_ui(ui, |ui| {
+                    for method in HttpMethod::iter() {
+                        ui.selectable_value(
+                            &mut request.method,
+                            method.clone(),
+                            method.to_string(),
+                        );
+                    }
+                });
+
+            ui.text_edit_singleline(&mut request.url);
+        });
+
+        ui.label("Headers:");
+        let height = egui::TextStyle::Body.resolve(ui.style()).size;
+        let mut to_remove = Vec::new();
+        egui_extras::TableBuilder::new(ui)
+            .striped(true)
+            .resizable(true)
+            .cell_layout(egui::Layout::left_to_right(egui::Align::LEFT))
+            .column(egui_extras::Column::remainder())
+            .column(egui_extras::Column::remainder())
+            .column(egui_extras::Column::auto())
+            .body(|mut body| {
+                for (idx, header) in request.headers.iter_mut().enumerate() {
+                    body.row(height, |mut row| {
+                        row.col(|ui| {
+                            ui.text_edit_singleline(&mut header.0);
+                        });
+                        row.col(|ui| {
+                            ui.text_edit_singleline(&mut header.1);
+                        });
+                        row.col(|ui| {
+                            if ui.button("❌").clicked() {
+                                to_remove.push(idx);
+                            }
+                        });
+                    });
+                }
+            });
+
+        for idx in to_remove.into_iter().rev() {
+            request.headers.remove(idx);
+        }
+
+        if ui.button("➕ Add Header").clicked() {
+            request.headers.push(("".to_owned(), "".to_owned()));
+        }
+
+        ui.label("Body:");
+        ui.text_edit_multiline(&mut request.body);
+    }
+
+    fn show_environment_editor(&mut self, ui: &mut eframe::egui::Ui, idx: usize) {
+        let allow_delete = self.environments.len() > 1;
+        let environment = &mut self.environments[idx];
+
+        ui.heading(&environment.name);
+
+        let mut delete_environment = false;
+        ui.horizontal(|ui| {
+            ui.text_edit_singleline(&mut environment.name);
+
+            if allow_delete && ui.button("❌").clicked() {
+                delete_environment = true;
+            }
+        });
+
+        if delete_environment {
+            self.environments.remove(idx);
+            self.selected_panel = PanelSelection::Environment(0);
+            return;
+        }
+
+        let mut to_remove = Vec::new();
+
+        eframe::egui::Grid::new("env_grid")
+            .min_col_width(ui.available_width() / 2.0)
+            .show(ui, |ui| {
+                for (idx, value) in environment.values.iter_mut().enumerate() {
+                    ui.text_edit_singleline(&mut value.0);
+                    ui.text_edit_singleline(&mut value.1);
+                    if ui.button("❌").clicked() {
+                        to_remove.push(idx);
+                    }
+                    ui.end_row();
+                }
+            });
+
+        for idx in to_remove.into_iter().rev() {
+            environment.values.remove(idx);
+        }
+
+        if ui.button("➕ Add Row").clicked() {
+            environment.values.push(("".to_owned(), "".to_owned()));
+        }
+    }
 }
 
 impl eframe::App for RequestieApp {
@@ -42,67 +197,65 @@ impl eframe::App for RequestieApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
                 egui::widgets::global_theme_preference_buttons(ui);
             });
         });
+        eframe::egui::SidePanel::left("left_panel").show(ctx, |ui| {
+            ui.heading("Requests");
+            let mut delete_idx: Option<usize> = None;
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("requestie");
+            for (idx, request) in self.requests.iter().enumerate() {
+                if ui
+                    .selectable_label(
+                        self.selected_panel == PanelSelection::Request(idx),
+                        &request.name,
+                    )
+                    .clicked()
+                {
+                    self.selected_panel = PanelSelection::Request(idx);
+                }
+            }
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            if ui.button("+ Request").clicked() {
+                self.requests.push(Request {
+                    name: format!("New Request {}", self.requests.len() + 1),
+                    url: "".to_string(),
+                    method: HttpMethod::GET,
+                    headers: vec![],
+                    body: "".to_string(),
+                });
+                self.selected_panel = PanelSelection::Request(self.requests.len() - 1);
             }
 
             ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+            ui.heading("Environments");
+            for (idx, env) in self.environments.iter().enumerate() {
+                if ui
+                    .selectable_label(
+                        self.selected_panel == PanelSelection::Environment(idx),
+                        &env.name,
+                    )
+                    .clicked()
+                {
+                    self.selected_panel = PanelSelection::Environment(idx);
+                }
+            }
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            if ui.button("+ Environment").clicked() {
+                self.environments.push(Environment {
+                    name: format!("New Environment {}", self.environments.len() + 1),
+                    values: vec![],
+                });
+                self.selected_panel = PanelSelection::Environment(self.environments.len() - 1);
+            }
+        });
+
+        eframe::egui::CentralPanel::default().show(ctx, |ui| match self.selected_panel {
+            PanelSelection::Request(idx) => self.show_request_editor(ui, idx),
+            PanelSelection::Environment(idx) => self.show_environment_editor(ui, idx),
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
